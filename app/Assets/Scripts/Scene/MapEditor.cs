82de1fcc959c8433fb135295d4763ca4b66a5ea8
Sergi Tortosa <tortosa95@gmail.com>
2019-04-22 20:11:04 +0200

    Map load on vr scene and ignores test props until they are needed on project

--- app/Assets/Scripts/Scene/MapEditor.cs
+++ app/Assets/Scripts/Scene/MapEditor.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MapEditor : MonoBehaviour{   
+public class MapEditor : MapLoader {   
     public enum MapMode{         
         Build,
         Edit,
@@ -24,8 +24,6 @@ public class MapEditor : MonoBehaviour{
     public Toggle DeleteToggle;
     public MapRaycast mapRaycast;
     public Transform objectList;
-    public Transform mapPivot;
-
 
 
     [Header("Map settings")]
@@ -71,7 +69,6 @@ public class MapEditor : MonoBehaviour{
             ResetCurrent(true, false);
         }
     }   
-    private GameObject items;
     private LayerMask currentObjectLayer;
     public LayerMask allExceptFloor = ~0;
 
@@ -125,94 +122,6 @@ public class MapEditor : MonoBehaviour{
 
         LoadObjectsButtons();
         LoadMap();
-    }
-
-    private void CreateRoot(){
-        if(items != null){
-            Destroy(items);
-        }
-
-        items = new GameObject();
-        items.transform.parent = mapPivot;
-        items.transform.localPosition = Vector3.zero;
-        items.name = "Items Container";
-    }
-
-    private void LoadMap(){
-        CreateRoot();
-
-        var loadedScene = SessionManager.Instance.loadedScene;
-
-        
-        var childs = new Dictionary<string, List<SceneGameObject>>();
-
-        foreach(var loadedSceneObject in loadedScene.objects){
-
-            Transform itemParent = items.transform;
-
-            SceneObject so = SessionManager.Instance.sceneObjects.GetObject(loadedSceneObject.ScriptableObjectGUID);
-
-            if(so == null){
-                Debug.LogWarning("Could not load SceneObject with this instance");
-                continue;
-            }
-
-            SceneGameObject scgo =  so.Instantiate(loadedSceneObject.AssignedNTVariable, itemParent, loadedSceneObject.position, Quaternion.Euler(loadedSceneObject.rotation));
-
-            if(!string.IsNullOrEmpty(loadedSceneObject.serializedGraph)){
-                scgo.graph = ScriptableObject.CreateInstance<SceneObjectGraph>();
-                scgo.graph.ImportSerialized(loadedSceneObject.serializedGraph);
-                scgo.graph.sceneVariables = SessionManager.Instance.sceneVariables;
-            }
-
-            if(!string.IsNullOrEmpty(loadedSceneObject.parent)){
-                SceneGameObject parentscgo = SessionManager.Instance.GetSceneGameObject(loadedSceneObject.parent);
-
-                if(parentscgo != null){
-                    scgo.transform.SetParent(parentscgo.transform);
-                    scgo.transform.localScale = Vector3.one;
-                    scgo.transform.localPosition = loadedSceneObject.position;
-                    scgo.transform.localRotation = Quaternion.Euler(loadedSceneObject.rotation);
-                }
-                else
-                {
-                    scgo.transform.localPosition = loadedSceneObject.position;
-                    scgo.transform.localRotation = Quaternion.Euler(loadedSceneObject.rotation);
-
-                    if(childs.ContainsKey(loadedSceneObject.parent)){
-                        List<SceneGameObject> childsForObj = childs[loadedSceneObject.parent];
-                        childsForObj.Add(scgo);
-                        childs[loadedSceneObject.parent] = childsForObj;
-                    }
-                    else
-                    {
-                        childs.Add(loadedSceneObject.parent, new List<SceneGameObject>(){ scgo });
-                    }
-                }                
-            }
-
-            if(childs.ContainsKey(loadedSceneObject.AssignedNTVariable)){
-                List<SceneGameObject> childsForObj = childs[loadedSceneObject.AssignedNTVariable];
-
-                foreach(SceneGameObject sgo in childsForObj){
-                    Vector3 localPos = sgo.transform.localPosition;
-                    Quaternion loclRot = sgo.transform.localRotation;
-
-                    sgo.transform.SetParent(scgo.transform);
-                    sgo.transform.localScale = Vector3.one;
-                    sgo.transform.localPosition = localPos;
-                    sgo.transform.localRotation = loclRot;
-                }
-
-                childs.Remove(loadedSceneObject.AssignedNTVariable);
-            }
-
-            SessionManager.Instance.AddSceneGameObject(scgo);
-        }
-
-        foreach(var ll in childs){
-            Debug.Log("sksksksk   " + ll.Key);
-        }
     }
 
     private void Update() {
---
+++ app/Assets/Scripts/Scene/MapLoader.cs
@@ -0,0 +1,104 @@
+using System.Collections.Generic;
+using NT.Graph;
+using NT.SceneObjects;
+using UnityEngine;
+
+public class MapLoader : MonoBehaviour {
+    public Transform mapPivot;
+    public GameObject items;
+
+
+    private void Start() {
+        SessionManager.Instance.OnSessionLoaded.AddListener(LoadMap); 
+        LoadMap();   
+    }
+
+    public void CreateRoot(){
+        if(items != null){
+            Destroy(items);
+        }
+
+        items = new GameObject();
+        items.transform.parent = mapPivot;
+        items.transform.localPosition = Vector3.zero;
+        items.name = "Items Container";
+    }
+
+    public void LoadMap(){
+        CreateRoot();
+
+        var loadedScene = SessionManager.Instance.loadedScene;
+
+        
+        var childs = new Dictionary<string, List<SceneGameObject>>();
+
+        foreach(var loadedSceneObject in loadedScene.objects){
+
+            Transform itemParent = items.transform;
+
+            SceneObject so = SessionManager.Instance.sceneObjects.GetObject(loadedSceneObject.ScriptableObjectGUID);
+
+            if(so == null){
+                Debug.LogWarning("Could not load SceneObject with this instance");
+                continue;
+            }
+
+            SceneGameObject scgo =  so.Instantiate(loadedSceneObject.AssignedNTVariable, itemParent, loadedSceneObject.position, Quaternion.Euler(loadedSceneObject.rotation));
+
+            if(!string.IsNullOrEmpty(loadedSceneObject.serializedGraph)){
+                scgo.graph = ScriptableObject.CreateInstance<SceneObjectGraph>();
+                scgo.graph.ImportSerialized(loadedSceneObject.serializedGraph);
+                scgo.graph.sceneVariables = SessionManager.Instance.sceneVariables;
+            }
+
+            if(!string.IsNullOrEmpty(loadedSceneObject.parent)){
+                SceneGameObject parentscgo = SessionManager.Instance.GetSceneGameObject(loadedSceneObject.parent);
+
+                if(parentscgo != null){
+                    scgo.transform.SetParent(parentscgo.transform);
+                    scgo.transform.localScale = Vector3.one;
+                    scgo.transform.localPosition = loadedSceneObject.position;
+                    scgo.transform.localRotation = Quaternion.Euler(loadedSceneObject.rotation);
+                }
+                else
+                {
+                    scgo.transform.localPosition = loadedSceneObject.position;
+                    scgo.transform.localRotation = Quaternion.Euler(loadedSceneObject.rotation);
+
+                    if(childs.ContainsKey(loadedSceneObject.parent)){
+                        List<SceneGameObject> childsForObj = childs[loadedSceneObject.parent];
+                        childsForObj.Add(scgo);
+                        childs[loadedSceneObject.parent] = childsForObj;
+                    }
+                    else
+                    {
+                        childs.Add(loadedSceneObject.parent, new List<SceneGameObject>(){ scgo });
+                    }
+                }                
+            }
+
+            if(childs.ContainsKey(loadedSceneObject.AssignedNTVariable)){
+                List<SceneGameObject> childsForObj = childs[loadedSceneObject.AssignedNTVariable];
+
+                foreach(SceneGameObject sgo in childsForObj){
+                    Vector3 localPos = sgo.transform.localPosition;
+                    Quaternion loclRot = sgo.transform.localRotation;
+
+                    sgo.transform.SetParent(scgo.transform);
+                    sgo.transform.localScale = Vector3.one;
+                    sgo.transform.localPosition = localPos;
+                    sgo.transform.localRotation = loclRot;
+                }
+
+                childs.Remove(loadedSceneObject.AssignedNTVariable);
+            }
+
+            SessionManager.Instance.AddSceneGameObject(scgo);
+        }
+
+        foreach(var ll in childs){
+            Debug.Log("sksksksk   " + ll.Key);
+        }
+    }
+
+}
